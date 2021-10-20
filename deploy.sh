a2b06a65cb26514f01f5cd7269684ddcfd840076
zhjnerv <47563214+zhjnerv@users.noreply.github.com>
2021-10-20 14:03:37 +0800

    修改部署网址

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'www.zhjwork.online\zhjwork.online' > CNAME
+echo 'zhjwork.online' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:zhjnerv/zhjworkonline.git
