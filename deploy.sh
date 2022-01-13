1cd4d6f6d24f02e4e78dda808c960b2dcbe05cdc
zhjnerv <47563214+zhjnerv@users.noreply.github.com>
2022-01-13 17:38:35 +0800

    不知道修了点什么东西

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ git push -f $githubUrl master:gh-pages # 推送到github
 #if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
 #  codingUrl=git@e.coding.net:zhjwork/zhjwork/zhjwork.git
 #else
-  codingUrl=https://HeupemlnXU:${CODING_TOKEN}@e.coding.net/zhjwork/zhjwork/zhjwork.git
+#  codingUrl=https://HeupemlnXU:${CODING_TOKEN}@e.coding.net/zhjwork/zhjwork/zhjwork.git
 #fi
 #git add -A
 #git commit -m "${msg}"
