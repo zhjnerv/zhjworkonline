0f5d8486002d1b2505e05933322300d11d030766
zhjnerv <zhjwork@gmail.com>
2023-02-16 00:26:34 +0800

    祛除了CNAME

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'zhjwork.online' > CNAME
+# echo 'zhjwork.online' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:zhjnerv/zhjworkonline.git
